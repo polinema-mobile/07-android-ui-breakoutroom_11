a5ebd646c91d8e11b850fba53b75f1db9cb43c18
raihanrach <raihanrach@gmail.com>
2020-09-30 15:42:03 +0700

    update

---
+++ app/src/main/java/com/example/a07androiduibreakoutroom_11/DataDiri.java
@@ -0,0 +1,14 @@
+package com.example.a07androiduibreakoutroom_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataDiri extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_diri);
+    }
+}
