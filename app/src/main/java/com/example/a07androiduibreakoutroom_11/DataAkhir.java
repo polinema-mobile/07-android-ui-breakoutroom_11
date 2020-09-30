1616ea70fe9245ef0a3c78975e0c21d5c3883c15
Nurhanifah99 <Hanifah110498@gmail.com>
2020-09-30 18:31:47 +0700

    coba1

---
+++ app/src/main/java/com/example/a07androiduibreakoutroom_11/DataAkhir.java
@@ -0,0 +1,14 @@
+package com.example.a07androiduibreakoutroom_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataAkhir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_akhir);
+    }
+}
